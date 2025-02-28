8c8bed06b89a125f461761677c2cc6874bba18e9
ckl117 <ckl117@163.com>
2025-02-28 17:13:59 +0800

    move tune_fp8_gemm.sh to csrc

--- csrc/tools/tune_fp8_gemm.sh
+++ csrc/tools/tune_fp8_gemm.sh
@@ -19,7 +19,7 @@ export FLAGS_use_cutlass_device_best_config_path=tune
 
 # 根据不同模型，调整gemm参数，其中n和k的的值需要成对出现，具体逻辑可参考相关.py文件。
 # llama3-8B 单GEMM调优参数设置
-# python ./utils/tune_cutlass_fp8_gemm.py \
+# python ../utils/tune_cutlass_fp8_gemm.py \
 #         --m_min 32 \
 #         --m_max 32768 \
 #         --n 6144 4096 4096 \
@@ -27,7 +27,7 @@ export FLAGS_use_cutlass_device_best_config_path=tune
 #         >  tune_fp8_gemm.log 2>&1
 
 # llama3-8B 双GEMM调优参数设置
-# python ./utils/tune_cutlass_fp8_dual_gemm.py \
+# python ../utils/tune_cutlass_fp8_dual_gemm.py \
 #         --m_min 32 \
 #         --m_max 32768 \
 #         --n 14336 \
@@ -35,7 +35,7 @@ export FLAGS_use_cutlass_device_best_config_path=tune
 #         >  tune_fp8_dual_gemm.log 2>&1
 
 # deepseek-v3 tp16调优
-# python ./utils/tune_cutlass_fp8_block_gemm.py \
+# python ../utils/tune_cutlass_fp8_block_gemm.py \
 #         --m_min 32 \
 #         --m_max 32768 \
 #         --n 1536 1536 576 2048 4096 512 7168 7168 2304 7168 256 7168 \
@@ -43,9 +43,9 @@ export FLAGS_use_cutlass_device_best_config_path=tune
 #         >  deepseekv3_fp8_config.log 2>&1 
 
 # fp8 gemm with scale tensor on sm90
-# python ./tune_cutlass_fp8_gemm_ptr_scale.py \
+# python ../utils/tune_cutlass_fp8_gemm_ptr_scale.py \
 #         --m_min 32 \
 #         --m_max 32768 \
 #         --n 6144 4096 4096 \
 #         --k 4096 4096 14336 \
-#         >  tune_fp8_gemm.log 2>&1
+#         >  tune_fp8_gemm_ptr_scale.log 2>&1
